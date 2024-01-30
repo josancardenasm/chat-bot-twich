b324fb1e17a569f6fad79eadf542708412a51b25
Jose Antonio Cárdenas Montero <joseantonio.cardenas@niko.eu>
2024-01-30 22:12:39 +0100

    Logs goes to a file.

--- main.cpp
+++ main.cpp
@@ -6,11 +6,41 @@
 #include <twichircclient.h>
 #include <twichapi.h>
 
-// #include <athorizationform.h>
+#include <QFile>
+#include <QTextStream>
+
+void messageHander(QtMsgType type, const QMessageLogContext& context, const QString& message) {
+    QString levelText;
+    switch (type) {
+    case QtDebugMsg:
+        levelText = "Debug";
+        break;
+    case QtInfoMsg:
+        levelText = "Info";
+        break;
+    case QtWarningMsg:
+        levelText = "Warning";
+        break;
+    case QtCriticalMsg:
+        levelText = "Critical";
+        break;
+    case QtFatalMsg:
+        levelText = "Fatal";
+        break;
+    }
+    QString text = QString("[%1] %2")
+                       .arg(levelText)
+                       .arg(message);
+    QFile file("app.log");
+    file.open(QIODevice::WriteOnly | QIODevice::Append);
+    QTextStream textStream(&file);
+    textStream << text << Qt::endl;
+}
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    //qInstallMessageHandler(messageHander);
 
     QQmlApplicationEngine engine;
 
